f4959d5cd2032c72ab386aefc0849460764a0d3c
unknown <samin.yr118@gmail.com>
2026-01-01 20:28:07 +0600

    port change back

--- Backend/nova-chrono-api/src/main.ts
+++ Backend/nova-chrono-api/src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.PORT ?? 2341;
   await app.listen(port);
   console.log(`Swagger link: http://localhost:${port}/api`);
 }
